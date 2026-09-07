72c3291a64f8b987950d3d27bf292a2052f6a4e2
thex-zar <ishmaelpampier4@gmail.com>
2026-09-07 11:16:42 +0200

    Fix import statements for Kivy modules

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
----
-from http://kivy.app import App
-from http://kivy.uix.label import Label
+from kivy.app import App
+from kivy.uix.label import Label
 class THEX(App):
     def build(self):
         return Label(text='THEX V2.5')
 THEX().run()
----
